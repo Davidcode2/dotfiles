b3437010260d585a5e14c31065ce44c9524258ef
Jakob Lingel <jlingel68@gmail.com>
2022-09-18 22:31:35 +0200

    Add code_actions for eslint

--- .config/nvim/null-ls.lua
+++ .config/nvim/null-ls.lua
@@ -7,18 +7,17 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
--- https://github.com/prettier-solidity/prettier-plugin-solidity
--- npm install --save-dev prettier prettier-plugin-solidity
 null_ls.setup {
   debug = false,
   sources = {
     formatting.prettier.with {
-      extra_filetypes = { "toml", "solidity" },
-      extra_args = { "--single-quote", "--jsx-single-quote" },
+      extra_filetypes = {  },
+      extra_args = { "--single-quote" },
     },
     diagnostics.eslint_d,
-    formatting.eslint_d
+    code_actions.eslint_d
     -- diagnostics.flake8,
   },
 }
