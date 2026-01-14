16136c6cf8fde6427ff2ee4fa915fb85ef061b77
Jakob Lingel <jlingel68@gmail.com>
2026-01-14 20:01:55 +0100

    switch to lazyvim

--- .config/nvim/dap/python.lua
+++
@@ -1,8 +0,0 @@
-local dap = require('dap')
-
--- python adapter
-dap.adapters.python = {
-  type = 'executable',
-  command = os.getenv('HOME') .. '/.virtualenvs/tools/bin/python',
-  args = { '-m', 'debugpy.adapter' },
-}
